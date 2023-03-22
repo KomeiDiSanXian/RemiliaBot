d519c0603915f908b673d7ad0349b1b00608d55a
KomeiDiSanXian <2211213431@qq.com>
2023-03-22 14:45:06 +0800

    修改zbputils

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ import (
 
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/manager" // 群管
 
-	_ "github.com/FloatTech/zbputils/job" // 定时指令触发器
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/job" // 定时指令触发器
 
 	//                               ^^^^                               //
 	//                          ^^^^^^^^^^^^^^                          //
