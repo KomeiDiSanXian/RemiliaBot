dcf2a0b8f6c7303a865e2189c8d768d49a3b4cda
KomeiDiSanXian <2211213431@qq.com>
2023-04-18 12:05:53 +0800

    edit banner

--- kanban/banner/banner.go
+++ kanban/banner/banner.go
@@ -3,14 +3,14 @@
 package banner
 
 // Version ...
-var Version = "1.7.1"
+var Version = "v1.7.1"
 
 // Copyright ...
 var Copyright = "© 2020 - 2023 FloatTech"
 
 // Banner ...
 var Banner = "* OneBot + ZeroBot + Golang\n" +
-	"* Version " + Version + " - 2023-04-18 11:36:47 +0800 CST\n" +
+	"* Version " + Version + " - 2023-04-18 12:05:16 +0800 CST\n" +
 	"* Copyright " + Copyright + ". All Rights Reserved.\n" +
 	"* Project: https://github.com/KomeiDiSanXian/RemiliaBot\n" +
 	"* Forked from: https://github.com/FloatTech/ZeroBot-Plugin"
