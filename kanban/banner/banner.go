a284349cb00d44ffaf7081052f24df9948741329
KomeiDiSanXian <2211213431@qq.com>
2024-05-12 17:15:25 +0800

    chore: Update copyright year in banner and version in winres.json

--- kanban/banner/banner.go
+++ kanban/banner/banner.go
@@ -6,11 +6,11 @@ package banner
 var Version = "v1.7.3"
 
 // Copyright ...
-var Copyright = "© 2020 - 2023 FloatTech"
+var Copyright = "© 2020 - 2024 FloatTech"
 
 // Banner ...
 var Banner = "* OneBot + ZeroBot + Golang\n" +
-	"* Version " + Version + " - 2023-08-02 01:22:12 +0800 CST\n" +
+	"* Version " + Version + " - 2024-05-12 17:02:19 +0800 CST\n" +
 	"* Copyright " + Copyright + ". All Rights Reserved.\n" +
 	"* Project: https://github.com/KomeiDiSanXian/RemiliaBot\n" +
 	"* Forked from: https://github.com/FloatTech/ZeroBot-Plugin"
